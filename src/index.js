f46dd840aa42dab8547217e4b56766724b339375
Amirsohil123 <amir77766618@gmail.com>
2023-01-23 15:18:25 +0530

    amir

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const route = require('./route/route');
 
 app.use(express.json());
 
-mongoose.connect("", {useNewUrlParser: true})
-.then(()=>console.log("Connected to database..."))
-.catch((err)=>console.log(err));
-
-const route = require('./route/route');
+mongoose.connect("mongodb+srv://SAURABH:Soa4GdK4yRvlVN5i@cluster0.umtgp.mongodb.net/group5Database?retryWrites=true&w=majority",
+    { useNewUrlParser: true })
+    .then(() => console.log("Connected to database..."))
+    .catch((err) => console.log(err));
 
 app.use('/', route);
 
-app.listen(3000,(err)=>{
-    if(err) console.log(err.message);
+app.listen(3000, (err) => {
+    if (err) console.log(err.message);
     console.log("Application is running on port 3000...");
 });
